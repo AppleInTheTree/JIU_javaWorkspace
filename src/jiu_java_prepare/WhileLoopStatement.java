f0bcbec8bb14c4ab5b20227e4ef8fc2b5d90ce40
AppleInTheTree <ejahn1175@gmail.com>
2023-01-16 16:10:01 +0700

    "preparing java class"

---
+++ src/jiu_java_prepare/WhileLoopStatement.java
@@ -0,0 +1,18 @@
+package jiu_java_prepare;
+
+public class WhileLoopStatement {
+    public static void main(String[] args) {
+
+        /*
+         * while(조건){
+         *   반복 실행 영역
+         * }
+         * */
+        int i  = 0;
+        while ( i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
