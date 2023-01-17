2eefe11b75f58b2f87327828d1265605e82cfbf2
AppleInTheTree <ejahn1175@gmail.com>
2023-01-17 16:55:46 +0700

    "JIU-JAVA-PREPARE DAY2"

---
+++ src/ac/jiu/javaclass/HomeworkTwoPone.java
@@ -0,0 +1,19 @@
+package ac.jiu.javaclass;
+
+public class HomeworkTwoPone {
+    public static void main(String[] args) {
+        double i = 50.0;
+        double k = i +50.0;
+        double j = k + 1;
+
+        System.out.println("J is" + j + " and k is" + k);
+
+    }
+
+}
+
+/*
+* 1. static
+* 2. String
+* "and" + "k is"
+*  */
