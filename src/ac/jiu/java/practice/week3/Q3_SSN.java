eb827334a1b65abc40c217f3ea6282ad2b87fda3
AppleInTheTree <ejahn1175@gmail.com>
2023-02-07 11:52:57 +0700

    Feat: Add week3 question3 Solve the problem three

--- src/ac/jiu/java/practice/week3/Q3_SSN.java
+++ src/ac/jiu/java/practice/week3/Q3_SSN.java
@@ -2,24 +2,28 @@ package ac.jiu.java.practice.week3;
 import java.util.Scanner;
 public class Q3_SSN {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan =  new Scanner(System.in);
         System.out.println("Enter a SSN: ");
         String ssn = scan.next();
 
         for (int i = 0; i < ssn.length(); i++) {
             char check = ssn.charAt(i);
-            if (i == 3 || i == 6) {
+            if (i == 3 || i ==6) {
                 if (!Character.isDigit(check)) {
-                    if (!(ssn.charAt(i) == '-')) {
-                        System.out.println(ssn + " is an invalid social security number");
-                        System.exit(1);
-                    } else {
+                    if (ssn.charAt(i) == '-') {
+                        continue;
+                    }
+                    else {
                         System.out.println(ssn + " is an invalid social security number");
                         System.exit(1);
                     }
                 }
+                else {
+                    System.out.println(ssn + " is an invalid social security number");
+                    System.exit(1);
+                }
             }
-            System.out.println(ssn + " is a valid social security number");
         }
+        System.out.println(ssn + " is a valid social security number");
     }
 }
