6553d7940862a12bd597a1ce220a48327ce5ed21
AppleInTheTree <ejahn1175@gmail.com>
2023-03-22 08:52:48 +0700

    Feat: Add grammer practice

---
+++ src/ac/jiu/java/grammer/chapter9/Test3.java
@@ -0,0 +1,14 @@
+package ac.jiu.java.grammer.chapter9;
+
+public class Test3 {
+    private final int hi;
+    private final int hello;
+
+    Test3() {
+        this(1,3);
+    }
+    Test3(int a, int b) {
+        this.hi = a;
+        this.hello = b;
+    }
+}
