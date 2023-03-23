ddcb2bcb4512c28581239b74c9b2347a944ced9a
AppleInTheTree <ejahn1175@gmail.com>
2023-03-23 21:54:25 +0700

    Feat: Add test3 practice

--- src/ac/jiu/java/grammer/chapter9/Test3.java
+++ src/ac/jiu/java/grammer/chapter9/Test3.java
@@ -3,6 +3,7 @@ package ac.jiu.java.grammer.chapter9;
 public class Test3 {
     private final int hi;
     private final int hello;
+    private int third;
 
     Test3() {
         this(1,3);
@@ -10,5 +11,10 @@ public class Test3 {
     Test3(int a, int b) {
         this.hi = a;
         this.hello = b;
+    }
+
+    Test3(int a, int b, int c) {
+        this(1, 4);
+        this.third = c;
     }
 }
