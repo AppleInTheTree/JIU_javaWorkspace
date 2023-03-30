d2680ed2708dbc6c50b542e91209bfc9f67185c3
AppleInTheTree <ejahn1175@gmail.com>
2023-03-30 19:36:33 +0700

    Feat: Add leetcode 291 question

---
+++ src/ac/jiu/java/leetcode/Q217_ContainsDuplicate.java
@@ -0,0 +1,42 @@
+package ac.jiu.java.leetcode;
+import java.util.*;
+public class Q217_ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 6, 7};
+
+        System.out.println(check(array));
+
+
+
+
+
+    }
+    public static boolean check(int[] array) {
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int indexValue = array[i];
+            int indexMin = i;
+            for(int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    //indexValue = array[j];
+                    indexMin = j;
+                }
+            }
+            if (indexMin != i) {
+                array[i] = array[indexMin];
+                array[indexMin] = indexValue;
+            }
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] == array[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+
+    }
+
+}
