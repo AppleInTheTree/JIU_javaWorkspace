9faf89c73a4465f16c5ebc7789ea81e0866ef836
AppleInTheTree <ejahn1175@gmail.com>
2023-04-04 12:04:50 +0700

    Feat: Revise the line indentation

--- src/ac/jiu/java/homework/assignment9/Q1_TestAirConditional.java
+++ src/ac/jiu/java/homework/assignment9/Q1_TestAirConditional.java
@@ -4,10 +4,12 @@ public class Q1_TestAirConditional {
     public static void main(String[] args) {
         AirConditioner conditioner1 = new AirConditioner();
         AirConditioner conditioner2 = new AirConditioner();
+
         //For conditional 1
         conditioner1.setTemperature(70);
         conditioner1.setSpeed(2);
         conditioner1.setOn(true);
+
         //For conditional 2
         conditioner2.setTemperature(68);
         conditioner2.setSpeed(3);
